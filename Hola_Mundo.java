b8b7205cfa0f4e6c8a3a8ab559aeb4bb5bd15944
youssef malki <ymalki20232@iesjoanramis.org>
2022-10-31 16:37:22 +0100

    calculo de a +b

--- Hola_Mundo.java
+++ Hola_Mundo.java
@@ -1,7 +1,12 @@
 public class Hola_Mundo {
+
     public static void main(String[] args) {
         System.out.println("Hola a todos, me llamo Youssef");
         //otro mensaje
         System.out.println("Formación incial Git");
+        //calculo de a y b
+        a=2; b=6;
+       System.out.println("El total de a +b es: "+a+b);
+
     }
 }
