3de9495231ee838bb4c5debfbd78fb9651a08b2c
youssef malki <ymalki20232@iesjoanramis.org>
2022-10-31 15:10:17 +0100

    primer test famoso de holaMundo segundo commit formación Git

--- Hola_Mundo.java
+++ Hola_Mundo.java
@@ -1,5 +1,7 @@
 public class Hola_Mundo {
     public static void main(String[] args) {
         System.out.println("Hola a todos, me llamo Youssef");
+        //otro mensaje
+        System.out.println("Formación incial Git");
     }
 }
